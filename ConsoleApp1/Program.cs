6e7f047fdaf194023b6ba1c7441293ac30565e28
SKB11 <shraddhakb1893@gmail.com>
2023-07-04 18:04:24 +0530

    Adding 2 numbers

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace program
+{
+    class Add
+    {
+        static void Main()
+        {
+            int n1, n2, sum;
+
+            Console.WriteLine("Enter the first number: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number: ");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            sum = n1 + n2;
+
+            Console.WriteLine("The sum of {0} and {1} is {2}", n1, n2, sum);
+
+            //Console.ReadKey();
+        }
+    }
+}
+
